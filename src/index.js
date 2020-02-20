2fea79f1536905c45b8fbc8bc285569b770626ff
Vladimir Bernacky <vladimir.bernacky@neotech.by>
2020-02-20 16:43:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let numb = number;
+    simple = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    hudred = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    function getTens(numb) {
+
+        if (numb <= 10) {
+            return simple[numb].toString();
+        }
+        if (numb > 10 && numb < 20) {
+            return simple[numb].toString();
+        }
+        if (numb >= 20 && numb < 100) {
+            return (hudred[Math.floor(numb / 10)] + (numb % 10 == 0 ? '' : ' ' + simple[numb % 10]));
+        }
+
+    }
+
+    if (numb < 100) {
+        return getTens(numb);
+    }
+
+    if (numb > 99) {
+        return ((simple[Math.floor(numb / 100)] + ' hundred') + (numb % 100 == 0 ? '' : ' ' + getTens(numb % 100)));
+    }
+
 }
